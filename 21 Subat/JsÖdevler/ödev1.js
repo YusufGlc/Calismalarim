99dd848766ffeabb0e5c80926826202d6e8366af
YusufGlc <yusufgulcu123@gmail.com>
2022-02-21 16:52:52 +0300

    21 Şubat

---
+++ 21 Subat/JsÖdevler/ödev1.js
@@ -0,0 +1,9 @@
+
+for (let index = 0; index < 100; index++) {
+
+    let randomSayi = Math.floor(Math.random()*100)
+    var toplam=0;
+    toplam+=randomSayi; 
+}
+
+console.log("100 rastgele sayının toplamının ortalaması "+toplam/100);
