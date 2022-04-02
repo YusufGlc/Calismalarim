a06896e69f801f977a929a96b3d549f83a1e3ddf
Yusuf Gülcü <yusufgulcu123@gmail.com>
2022-04-02 07:22:59 +0300

    2 Nisan

--- 21 Mart/Arex Sigorta/arex.js
+++ 21 Mart/Arex Sigorta/arex.js
@@ -23,6 +23,8 @@ function dropdownClick() {
 
 }
 
+
+// Overly
 function overlyShow() {
     let overly = document.querySelector(".overly");
     overly.style.display="block";
@@ -34,8 +36,9 @@ function overlyHide() {
 }
 
 
-
+// 
 function urunlerHover() {
+    closeAllMenu();
     overlyShow();
     let box = document.querySelector(".urunler-box");
     box.style.display="block";
@@ -44,9 +47,80 @@ function urunlerHover() {
     urunlerLink.appendChild(box);
 }
 
-function urunlerLeave() {
+function kurumsalHover() {
+    closeAllMenu();
+    overlyShow();
+    let box = document.querySelector(".kurumsal-box");
+    box.style.display="block";
+
+    let urunlerLink = document.querySelector(".kurumsal-a");
+    urunlerLink.appendChild(box);
+}
+
+function acenteHover() {
+    closeAllMenu();
+    overlyShow();
+    let box = document.querySelector(".acente-box");
+    box.style.display="block";
+
+    let urunlerLink = document.querySelector(".acente-a");
+    urunlerLink.appendChild(box);
+}
+
+function hasarYardimHover() {
+    closeAllMenu();
+    overlyShow();
+    let box = document.querySelector(".hasarYardım-box");
+    box.style.display="block";
+
+    let urunlerLink = document.querySelector(".hasarYardım-a");
+    urunlerLink.appendChild(box);
+}
+
+function kesfetHover() {
+    closeAllMenu();
+    overlyShow();
+    let box = document.querySelector(".kesfet-box");
+    box.style.display="block";
+
+    let urunlerLink = document.querySelector(".kesfet-a");
+    urunlerLink.appendChild(box);
+}
+
+function closeAllMenu() {
     overlyHide();
-    let box = document.querySelector(".urunler-box");
-    box.style.display="none";
+    let box = document.querySelectorAll(".hidden-box");
+    box.forEach(element => {
+        element.style.display="none";
+    });
 
 }
+
+
+// Navbar
+
+window.onscroll = function name() { 
+
+    if (window.scrollY != 0) {
+        
+        let nav = document.querySelector(".navbar")
+
+        nav.setAttribute('style', 'position:fixed !important; box-shadow: 0 1px 25px 1px gray;');
+
+        let banner = document.querySelector(".banner");
+        banner.setAttribute('style','margin-top:75px');
+    }
+    else{
+
+        let nav = document.querySelector(".navbar")
+
+        nav.setAttribute('style', 'box-shadow:none');
+
+        let banner = document.querySelector(".banner");
+
+        banner.setAttribute('style','margin-top:15px');
+
+    }
+
+}
+
